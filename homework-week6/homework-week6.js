e22863454555c1dcaf26654d1fa3d3e9d9958c2d
Yiting-B <ybao00929@gmail.com>
2025-04-11 13:40:16 +0200

    homework-week6

---
+++ homework-week6/homework-week6.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let count = 0;
+  const plusButton = document.getElementById("plus");
+  const minusButton = document.getElementById("minus");
+  const resetButton = document.getElementById("reset");
+  const numberTxt = document.querySelector(".part1");
+  const freeTips = document.getElementById("free");
+  const soldOut = document.getElementById("pause");
+
+  function sameFunction() {
+    numberTxt.textContent = count;
+    freeTips.style.display = count >= 10 && count < 20 ? "block" : "none";
+    soldOut.style.display = count >= 20 ? "block" : "none";
+    plusButton.style.backgroundColor = count >= 20 ? "red" : "";
+    plusButton.disabled = count>=20 ? "true" : "false";
+    minusButton.disabled = count<= 0 ? "true" : "false";
+  }
+  plusButton.addEventListener("click", () => {
+    count++;
+    sameFunction();
+  });
+  minusButton.addEventListener("click", () => {
+    count--;
+    sameFunction();
+  });
+  resetButton.addEventListener("click", () => {
+    count = 0;
+    sameFunction();
+  });
+});
